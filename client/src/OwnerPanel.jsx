f0a8ac7f426712931212b5020966c80694ac0440
sravya srinivas <sasidhar218@gmail.com>
2026-08-18 01:47:52 +0530

    Fix owner panel API for deployment

--- client/src/OwnerPanel.jsx
+++ client/src/OwnerPanel.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API = "http://localhost:5000";
+const API = "https://roof-estimator-server.onrender.com";
 
 function OwnerPanel() {
   const [username, setUsername] = useState("");
